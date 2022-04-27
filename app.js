b68e219e914501242efdfb3c699d07648bf4fccd
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-27 11:06:24 -0500

    Step 6: Adding new route with Query Params (req.params)

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.get("/explorersInNode", (req, res) => {
     res.send(explorer)
 })
 
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/fernando
+// req.params = {"explorerName": "fernando"}
+app.get("/explorers/:MiNombreEs", (req, res) => {
+    res.send(req.params)
+})
+
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
